beba0ea060f9493b84554d173ab2d0b48cf641cb
deBath <alex.solovov@list.ru>
2023-06-27 14:34:07 +0300

    add a second try

---
+++ try2/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let mut name = String::from("Alexey");
+    let age = 19;
+    let mut num = 93;
+    num = 13;
+    println!("Hello, my name is {} i'm {} y.o. and my fav number is {}", name, age, num)
+}
